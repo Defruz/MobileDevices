5e610f884f95d9fed05be0bd474543f6cc86ec6c
david.defrutos.zafra-D <david.defrutos.zafra@alumnos.upm.es>
2021-12-30 19:34:56 +0100

    madre mia toda la tarde

---
+++ ArticuloHeaders.java
@@ -0,0 +1,124 @@
+import com.google.gson.annotations.SerializedName;
+
+public class ArticuloHeaders {
+    @SerializedName(value = "id")
+	private String id;
+	@SerializedName(value = "id_user")
+	private String  id_user; 
+	@SerializedName(value = "is_public")
+	private String  is_public; 
+	@SerializedName(value = "abstract")
+	private String  abstracto;
+    @SerializedName(value = "body")
+	private String  body;
+    @SerializedName(value = "subtitle")
+	private String  subtitle;
+    @SerializedName(value = "update_date")
+	private String  update_date;
+    @SerializedName(value = "category")
+	private String  category;
+    @SerializedName(value = "title")
+	private String  title;
+    @SerializedName(value = "is_deleted")
+	private String  is_deleted;
+    @SerializedName(value = "thumbnail_image")
+	private String  thumbnail_image;
+    @SerializedName(value = "username")
+	private String  username;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId_User() {
+		return id_user;
+	}
+
+	public void setId_User(String id_user) {
+		this.id_user = id_user;
+	}
+
+	public String getIs_public() {
+		return is_public;
+	}
+
+	public void setIs_public(String is_public) {
+		this.is_public = is_public;
+	}
+
+	public String getAbstract() {
+		return abstracto;
+	}
+
+	public void setAbstract(String abstracto) {
+		this.abstracto = abstracto;
+	}
+
+    public String getBody() {
+		return body;
+	}
+
+	public void setbody(String body) {
+		this.body = body;
+	}
+
+    public String getSubtitle() {
+		return subtitle;
+	}
+
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+
+    public String getUpdate_date() {
+		return update_date;
+	}
+
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+
+    public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+    public String getIs_deleted() {
+		return is_deleted;
+	}
+
+	public void setIs_deleted(String is_deleted) {
+		this.is_deleted = is_deleted;
+	}
+
+    public String getThumbnail_image() {
+		return thumbnail_image;
+	}
+
+	public void setThumbnail_image(String thumbnail_image) {
+		this.thumbnail_image = thumbnail_image;
+	}
+
+    public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
--- Post.java
+++ Post.java
@@ -6,10 +6,13 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.stream.IntStream;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,12 +23,11 @@ import com.google.gson.JsonObject;
  */
 public class Post {
 
-    public static void login(JsonObject userCredentials) {
-        String cuerpo = "Texto";
-        URL myURL;
+    public static String login(URL myURL, JsonObject userCredentials) {
+
+        String resultado = "";
         try {
             // realizar conexion
-            myURL = new URL("https://sanger.dia.fi.upm.es/pmd-task/login");
             HttpURLConnection myURLConnection = (HttpURLConnection) myURL.openConnection();
             myURLConnection.setRequestMethod("POST");
             myURLConnection.setUseCaches(false);
@@ -34,43 +36,105 @@ public class Post {
 
             myURLConnection.setRequestProperty("Content-Type", "application/json");
             myURLConnection.connect();
-
+             
+            // enviar informacion del usuario
             OutputStream os = myURLConnection.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
             osw.write(userCredentials.toString());
             osw.flush();
             osw.close();
 
+            // leer lo devuelto por el servidor
             InputStream is = myURLConnection.getInputStream();
 
             String result = "";
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(is));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line = null;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
                 result += line;
             }
             reader.close();
 
+            // convertir a formato json el string obtenido como respuesta del servidor
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			PostHeaders resEnJava = gson.fromJson(result,PostHeaders.class);
-			
-			System.out.println(resEnJava.getApikey());
+            PostHeaders resEnJava = gson.fromJson(result, PostHeaders.class);
+            resultado = gson.toJson(resEnJava);
+
+            return resultado;
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return resultado;
+    }
+
+    public static String consulta(URL urlConsulta) {
+        String resultado = "";
+        try {
+            String[] url = urlConsulta.toString().split("/");
+            int limit = Integer.valueOf(url[url.length - 2]);
+            int offset = Integer.valueOf(url[url.length - 1]);
+
+            // realizar conexion
+            HttpURLConnection myURLConnection = (HttpURLConnection) urlConsulta.openConnection();
+            myURLConnection.setRequestMethod("GET");
+            myURLConnection.setUseCaches(false);
+            myURLConnection.setDoInput(true);
+            myURLConnection.setDoOutput(true);
+
+            myURLConnection.setRequestProperty("Content-Type", "application/json");
+            myURLConnection.connect();
+
+             // leer lo devuelto por el servidor en la peticion del get
+            InputStream is = myURLConnection.getInputStream();
+
+            String result = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                result += line;
+            }
+            reader.close();
+
+            // convertir a formato json el string obtenido como respuesta del servidor
+            // crear array copia donde almacenar los articulos a partir del offset indicado
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            ArticuloHeaders[] article = gson.fromJson(result, ArticuloHeaders[].class);
+            ArticuloHeaders[] copia = new ArticuloHeaders[limit - offset + 1];
+
+            int i = 0;
+            while (offset <= article.length) {
+                copia[i] = article[offset - 1];
+                offset++;
+                i++;
+            }
+            resultado = gson.toJson(copia);
+            return resultado;
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultado;
     }
 
     public static void main(String[] args) {
+        URL myURLlogin, urlConsulta;
+        try {
+            myURLlogin = new URL("https://sanger.dia.fi.upm.es/pmd-task/login");
+            urlConsulta = new URL("https://sanger.dia.fi.upm.es/pmd-task/articles/3/2");
+            JsonObject userCredentials = new JsonObject();
+            userCredentials.addProperty("username", "GR_16");
+            userCredentials.addProperty("passwd", "332002");
+            // System.out.println(login(myURLlogin, userCredentials));
+            System.out.println(consulta(urlConsulta));
 
-        JsonObject userCredentials = new JsonObject();
-        userCredentials.addProperty("username", "GR_16");
-        userCredentials.addProperty("passwd", "332002");
-        login(userCredentials);
-
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
